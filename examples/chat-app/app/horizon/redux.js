9bf961c5889bec1c1b1c784fde743e3548749b5f
Shane Cavaliere <shane@shanecavaliere.com>
2016-08-11 15:21:36 -0600

    fixed some chat-app example issues

--- examples/chat-app/app/horizon/redux.js
+++ examples/chat-app/app/horizon/redux.js
@@ -1,11 +1,5 @@
 import horizon from './connection'
-
-let HorizonRedux
-if (process.env.USE_LIB === true) {
-  HorizonRedux = require('../../../../lib').default
-} else {
-  HorizonRedux = require('../../../../src').default
-}
+import HorizonRedux from '../../../../src'
 
 const horizonRedux = HorizonRedux(horizon)
 
